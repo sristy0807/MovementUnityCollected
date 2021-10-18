9dd37767074bee87062a8624343b33882419c6c6
Sheikh Saad Sristy <sristy.cse@gmail.com>
2021-10-18 22:09:05 +0600

    movement all codes

---
+++ Movement in Unity Final/Assets/RW/2D Platformer Movement/Scripts/CattoMovement.cs
@@ -0,0 +1,113 @@
+﻿/*
+ * Copyright (c) 2020 Razeware LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
+ * distribute, sublicense, create a derivative work, and/or sell copies of the 
+ * Software in any work that is designed, intended, or marketed for pedagogical or 
+ * instructional purposes related to programming, coding, application development, 
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works, 
+ * or sale is expressly withheld.
+ *    
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using UnityEngine;
+
+public class CattoMovement : MonoBehaviour
+{
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask ground;
+    public float moveInput;
+    public float cattoSpeed;
+    public float cattoJumpForce;
+
+    private Rigidbody2D cattoRigidbody2D;
+    private Animator cattoAnimator;
+
+    private bool cattoIsFacingRight = true;
+    private bool cattoIsJumping = false;
+    private bool cattoIsGrounded = false;
+
+    void Start()
+    {
+        cattoRigidbody2D = GetComponent<Rigidbody2D>();
+        cattoAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        cattoIsGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, ground);
+
+        moveInput = Input.GetAxis("Horizontal");
+
+        if (cattoIsGrounded)
+        {
+            cattoAnimator.SetFloat("Velocity", Mathf.Abs(moveInput));
+        }
+
+        if (Input.GetButtonDown ("Jump") && cattoIsGrounded)
+        {
+            cattoIsJumping = true;
+            cattoAnimator.SetTrigger("Jump");
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) && cattoIsGrounded)
+        {
+            cattoAnimator.SetBool("Crouch", true);
+        }
+
+        else if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            cattoAnimator.SetBool("Crouch", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        cattoRigidbody2D.velocity = new Vector2(moveInput * cattoSpeed, cattoRigidbody2D.velocity.y);
+
+        if (cattoIsFacingRight == false && moveInput > 0)
+        {
+            FlipCatto();
+        }
+        else if (cattoIsFacingRight == true && moveInput < 0)
+        {
+            FlipCatto();
+        }
+
+        if (cattoIsJumping)
+        {
+            cattoRigidbody2D.AddForce(new Vector2(0f, cattoJumpForce));
+
+            cattoIsJumping = false;
+        }
+    }
+
+    private void FlipCatto()
+    {
+        cattoIsFacingRight = !cattoIsFacingRight;
+        
+        Vector3 cattoScale = transform.localScale;
+        cattoScale.x *= -1;
+
+        transform.localScale = cattoScale;
+    }
+}
---
+++ Movement in Unity Final/Assets/RW/3D Tank Movement/Scripts/Missile.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * Copyright (c) 2020 Razeware LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
+ * distribute, sublicense, create a derivative work, and/or sell copies of the 
+ * Software in any work that is designed, intended, or marketed for pedagogical or 
+ * instructional purposes related to programming, coding, application development, 
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works, 
+ * or sale is expressly withheld.
+ *    
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    public float bulletSpeed = 100f;
+    public GameObject explosionPrefab;
+    public float blastRadius = 5f;
+
+    private Rigidbody rbInstanceBullet;
+
+    void Start()
+    {
+        rbInstanceBullet = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        rbInstanceBullet.velocity = rbInstanceBullet.transform.forward * bulletSpeed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contact = collision.GetContact(0);
+        Vector3 contactPosition = contact.point;
+
+        var explosionInstance = Instantiate(explosionPrefab, contactPosition, Quaternion.identity);
+
+        ParticleSystem particlesOfExplosion = explosionInstance.GetComponent<ParticleSystem>();
+        particlesOfExplosion.Play();
+
+        if (collision.gameObject.CompareTag("TankRatto"))
+        {
+            Rigidbody ratto = collision.gameObject.GetComponent<Rigidbody>();
+            ratto.AddExplosionForce(1000, transform.position, blastRadius);
+        }
+
+        Physics.OverlapSphere(transform.position, blastRadius);
+
+        Destroy(gameObject, particlesOfExplosion.main.duration);
+        Destroy(explosionInstance, particlesOfExplosion.main.duration);
+    }
+}
